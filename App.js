0178de6f67535efae3d9eb32f7fc77295cd56948
btjdghks2 <btjdghks2@gmail.com>
2023-01-23 07:42:19 +0900

    test

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View,TextInput, TouchableOpacity, Image } from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Category } from './component/Category';
 
 export default function App() {
+  const [text, onChangeText] = useState();
+
+  const Stack = createNativeStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <View style={{flex:0.7}}>
+            <StatusBar style="auto" />
+      <View style={{flex:0.4 ,backgroundColor:'blue'}}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      /></View>
+      <View style={{flex:0.2 ,backgroundColor:'white',flexDirection: "row" , justifyContent:'space-around'}}>    
+      <NavigationContainer>
+        <Stack.Screen
+          name="Category"
+          component={Category}
+          options={{title: 'Welcome'}}
+        />
+        
+        
+    </NavigationContainer>
+    <TouchableOpacity onPress={Category} >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>건축자제</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={Category} >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>가구</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={Category} >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>데스크셋</Text>
+          </View>
+        </TouchableOpacity>
+        
+    </View>
+      <View style={{flex:1 ,backgroundColor:'red'}}><Image source={require('./interior.jpg')} /></View>
+      <View style={{flex:1 ,backgroundColor:'pink'}}><Text>네번째 칸입니다</Text></View>
+
     </View>
   );
 }
@@ -13,8 +58,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    marginTop: 35,
+    marginLeft: 10,
+    marginRight: 10,
+    borderWidth: 1,
+  },
+  button: {
+    width: 100,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2196F3',
+    borderRadius : 15,
+
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    marginTop:2,
+    fontSize: 22,
   },
 });
